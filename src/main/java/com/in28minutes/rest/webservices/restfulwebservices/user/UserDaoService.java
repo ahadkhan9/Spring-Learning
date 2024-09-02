57f6a6a16fa08caf4838f69f63b7fee6e7a207e5
Ahad Khan <ahadkhan5547@gmail.com>
2024-09-02 17:05:07 +0530

    just checking

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -12,7 +12,7 @@ public class UserDaoService {
     public List<User> users = new ArrayList<>();
     public UserDaoService() {
 
-        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(1, "ahad", LocalDate.now().minusYears(30)));
         users.add(new User(2, "Eve", LocalDate.now().minusYears(20)));
         users.add(new User(3, "Jack", LocalDate.now().minusYears(10)));
         users.add(new User(4, "Jill", LocalDate.now().minusYears(5)));
